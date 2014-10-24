fe07eee96f776c66b931f2d7b6dabbd99fad0b84
Liam Newmarch <liam@liamnewmarch.co.uk>
2014-10-24 16:45:21 +0100

    Add comments, minor clean up

--- main.js
+++ main.js
@@ -2,32 +2,55 @@
 
     'use strict';
 
+
     var HackerNews = angular.module('app', ['firebase']);
 
+
+    /**
+     * URLProvider
+     *
+     * Provides URLs for the API, and other end points.
+     */
+
     HackerNews.service('URLProvider', function() {
+
+        // Base URLs
         var baseAPI = 'https://hacker-news.firebaseio.com/v0/',
             baseHN = 'https://news.ycombinator.com/';
 
+        // A story, comment, poll, etc
         this.item = function(id) {
             return baseAPI + 'item/' + id;
         };
 
+        // Array of top story IDs
         this.top = function() {
             return baseAPI + 'topstories';
         };
 
+        // View an item on HN
         this.view = function(id) {
             return baseHN + 'item?id=$1';
         };
 
+        // Cast a vote on HN (requires login)
         this.vote = function(id) {
             return baseHN + 'vote?for=' + id + '&dir=up&whence=news';
         };
     });
 
+
+    /**
+     * HackerNewsAPI
+     *
+     * Handles connection and requests to the API.
+     *
+     */
     HackerNews.service('HackerNewsAPI', function($firebase, $q) {
+
+        // Request a specific url and return a promise
         this.fetch = function(url) {
-            var promise, connection, onValue;
+            var connection, onValue;
 
             return $q(function(resolve) {
 
@@ -39,13 +62,23 @@
             });
         };
 
+        // Close the API connection
         this.disconnect = function() {
             Firebase.goOffline();
         };
     });
 
+
+    /**
+     * TopStories
+     *
+     * Requests the top story IDs, then for each ID requests the story item, all
+     * wrapped up in promises.
+     */
+
     HackerNews.service('TopStories', function($q, HackerNewsAPI, URLProvider) {
 
+        // Fetch the top stories and triggers fetchStory for each one
         function fetchTop(onStory, onDone) {
             HackerNewsAPI.fetch(URLProvider.top()).then(function(snapshot) {
                 var promises = [];
@@ -62,13 +95,15 @@
             });
         }
 
+        // Fetch the story item for a given ID
         function fetchStory(id, onStory) {
-
             HackerNewsAPI.fetch(URLProvider.item(id)).then(function(snapshot) {
                 var story = snapshot.val();
 
+                // JS expects miliseconds but the API provides seconds
                 story.date = new Date(story.time * 1000);
 
+                // If this story has text update it’s url to point back to HN
                 if (story.text) {
                     story.url = URLProvider.view(story.id);
                 }
@@ -77,6 +112,7 @@
             });
         }
 
+        // Provides a controller hook to listen for individual story loads
         this.each = function(onStory) {
             return $q(function(resolve) {
                 fetchTop(function(story) {
@@ -85,14 +121,25 @@
             });
         };
 
+        // Provides a controller hook to close the connection
         this.disconnect = function() {
             HackerNewsAPI.disconnect();
         };
     });
 
+    /**
+     * TopStoriesCtrl
+     *
+     * Controller logic for the main view, triggers TopStories then closes the
+     * connection when we have them all.
+     *
+     */
     HackerNews.controller('TopStoriesCtrl', function($scope, TopStories) {
 
+        // Holds the top stories
         $scope.stories = [];
+
+        // Fetch top stories
         TopStories.each(function(story) {
             $scope.stories.push(story);
         }).then(function() {
@@ -100,6 +147,13 @@
         });
     });
 
+
+    /**
+     * domain
+     *
+     * Filter to get the domain name from a full URL string.
+     *
+     */
     HackerNews.filter('domain', function () {
         return function(url) {
             var a = document.createElement('a');
