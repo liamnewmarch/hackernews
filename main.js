3cf034f25706ca5db4b7e3a14559541484673c0e
Liam Newmarch <liam@liamnewmarch.co.uk>
2014-10-24 15:41:20 +0100

    Move logic from controller to services

--- main.js
+++ main.js
@@ -2,59 +2,105 @@
 
     'use strict';
 
-    var url = {
-        base: 'https://news.ycombinator.com/',
-        api: 'https://hacker-news.firebaseio.com/v0/'
-    };
+    var HackerNews = angular.module('app', ['firebase']);
 
-    var App = angular.module('app', ['firebase']);
+    HackerNews.service('URLProvider', function() {
+        var baseAPI = 'https://hacker-news.firebaseio.com/v0/',
+            baseHN = 'https://news.ycombinator.com/';
 
-    App.controller('TopStoriesCtrl', function($scope, $firebase, $q) {
+        this.item = function(id) {
+            return baseAPI + 'item/' + id;
+        };
 
-        var topListener, topReference;
+        this.top = function() {
+            return baseAPI + 'topstories';
+        };
+
+        this.view = function(id) {
+            return baseHN + 'item?id=$1';
+        };
+
+        this.vote = function(id) {
+            return baseHN + 'vote?for=' + id + '&dir=up&whence=news';
+        };
+    });
+
+    HackerNews.service('HackerNewsAPI', function($firebase, $q) {
+        this.fetch = function(url) {
+            var promise, connection, onValue;
+
+            return $q(function(resolve) {
+
+                connection = new Firebase(url);
+                onValue = connection.on('value', function(snapshot) {
+                    resolve(snapshot);
+                    connection.off('value', onValue);
+                });
+            });
+        };
+
+        this.disconnect = function() {
+            Firebase.goOffline();
+        };
+    });
+
+    HackerNews.service('TopStories', function($q, HackerNewsAPI, URLProvider) {
+
+        function fetchTop(onStory, onDone) {
+            HackerNewsAPI.fetch(URLProvider.top()).then(function(snapshot) {
+                var promises = [];
+
+                angular.forEach(snapshot.val(), function(id) {
+                    var promise = $q(function(resolve) {
+                        fetchStory(id, resolve);
+                    });
+                    promise.then(onStory);
+                    promises.push(promise);
+                });
+
+                $q.all(promises).then(onDone);
+            });
+        }
+
+        function fetchStory(id, onStory) {
+
+            HackerNewsAPI.fetch(URLProvider.item(id)).then(function(snapshot) {
+                var story = snapshot.val();
+
+                story.date = new Date(story.time * 1000);
+
+                if (story.text) {
+                    story.url = URLProvider.view(story.id);
+                }
+
+                onStory(story);
+            });
+        }
+
+        this.each = function(onStory) {
+            return $q(function(resolve) {
+                fetchTop(function(story) {
+                    onStory(story);
+                }, resolve);
+            });
+        };
+
+        this.disconnect = function() {
+            HackerNewsAPI.disconnect();
+        };
+    });
+
+    HackerNews.controller('TopStoriesCtrl', function($scope, TopStories) {
 
         $scope.stories = [];
-        topReference = new Firebase(url.api + 'topstories');
-
-        topListener = topReference.on('value', function(topSnapshot) {
-
-            var promises = [],
-                top = topSnapshot.val();
-
-            angular.forEach(top, function(id) {
-
-                promises.push($q(function(resolve) {
-
-                    var storyListener,
-                        storyReference = new Firebase(url.api + 'item/' + id);
-
-                    storyListener = storyReference.on('value', function(storySnapshot) {
-
-                        var story = storySnapshot.val();
-
-                        story.date = new Date(story.time * 1000);
-                        $scope.stories.push(story);
-                        $scope.$apply();
-
-                        if (story.text) {
-                            story.url = url.base + 'item?id=' + story.id;
-                        }
-
-                        storyReference.off('value', storyListener);
-                        resolve();
-                    });
-                }));
-            });
-
-            $q.all(promises).then(function() {
-                Firebase.goOffline();
-            });
-
-            topReference.off('value', topListener);
+        TopStories.each(function(story) {
+            $scope.stories.push(story);
+        }).then(function() {
+            TopStories.disconnect();
         });
     });
 
-    App.filter('domain', function () {
+    HackerNews.filter('domain', function () {
         return function(url) {
             var a = document.createElement('a');
             a.href = url;
