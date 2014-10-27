abeefd9a5f57ef080953475f2699eea80a1ab5b7
Liam Newmarch <liam@liamnewmarch.co.uk>
2014-10-27 16:41:05 +0000

    Setup karma (via grunt)

---
+++ js/test/unit/controllers/top-storiesSpec.js
@@ -0,0 +1,17 @@
+describe('TopStoriesCtrl', function() {
+
+    beforeEach(module('app'));
+
+    it('should create stories', inject(function($controller) {
+
+		var scope = {};
+
+        $controller('TopStoriesCtrl', {
+            $scope: scope
+        });
+
+        expect(scope.stories.length).toBe(0);
+
+    }));
+
+});
